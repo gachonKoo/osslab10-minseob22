a98d7130894c19419420343f4c759f997a979efc
mshan-brm99 <140988048+minseob22@users.noreply.github.com>
2024-11-10 04:31:58 +0900

    Update divisor.py

--- divisor.py
+++ divisor.py
@@ -2,7 +2,7 @@ import sys
 
 number = int(sys.argv[1])
 
-for i in range(1, number + 1):  # loop between 1 and number
+for i in range(0, number):  # loop between 1 and number
     if number % i == 0:  # check if remainder is 0
         print(i, end=" ")
 
